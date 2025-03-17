412e954df43c6166e708051298be3414cd78d646
Mark Canlas <github@htmlism.com>
2025-03-16 22:58:52 -0400

    partial

--- stable-matching-data/src/main/scala/com/htmlism/stablematching/data/OrderedBiMap.scala
+++ stable-matching-data/src/main/scala/com/htmlism/stablematching/data/OrderedBiMap.scala
@@ -3,9 +3,11 @@ package com.htmlism.stablematching.data
 import cats.Eq
 
 /**
- * Used when the population is split in half, type A for proposers, and type B for acceptors.
- *
- * @tparam A Proposer type
- * @tparam B Acceptor type
- */
-case class OrderedBiMap[A : Eq, B : Eq]()
+  * Used when the population is split in half, type A for proposers, and type B for acceptors.
+  *
+  * @tparam A
+  *   Proposer type
+  * @tparam B
+  *   Acceptor type
+  */
+case class OrderedBiMap[A: Eq, B: Eq]()
--- stable-matching-data/src/main/scala/com/htmlism/stablematching/data/OrderedStableMatching.scala
+++ stable-matching-data/src/main/scala/com/htmlism/stablematching/data/OrderedStableMatching.scala
@@ -1,10 +1,33 @@
 package com.htmlism.stablematching.data
 
 import cats.Eq
+import cats.syntax.all.*
 
 /**
- * Used when the population matches to itself, as in the stable roommates problem
- *
- * @tparam A The roommate type
- */
-case class OrderedStableMatching[A : Eq]()
+  * Used when the population matches to itself, as in the stable roommates problem
+  *
+  * @tparam A
+  *   The roommate type
+  */
+case class OrderedStableMatching[A: Eq]()
+
+object OrderedStableMatching:
+  case class Total[A: Eq](population: List[A], mapping: Map[A, A]):
+    assert(population.size == mapping.size)
+
+  case class Partial[A: Eq](population: List[A], mapping: Map[A, A]):
+    def withMatching(x: A, y: A): Either[String, Partial[A]] =
+      Either
+        .cond(
+          x =!= y,
+          copy(
+            mapping = mapping
+              .updated(x, y)
+              .updated(y, x)
+          ),
+          "x cannot equal y in a matching",
+        )
+
+  object Partial:
+    def empty[A: Eq](population: List[A]): Partial[A] =
+      Partial(population, Map.empty)
