09edc379679dc9e5b558c43d2f5f0923ee21dc37
Mark Canlas <github@htmlism.com>
2025-03-21 20:48:29 -0400

    doc

--- stable-matching-data/src/main/scala/com/htmlism/stablematching/data/OrderedStableMatching.scala
+++ stable-matching-data/src/main/scala/com/htmlism/stablematching/data/OrderedStableMatching.scala
@@ -3,18 +3,26 @@ package com.htmlism.stablematching.data
 import cats.Eq
 import cats.syntax.all.*
 
-/**
-  * Used when the population matches to itself, as in the stable roommates problem
-  *
-  * @tparam A
-  *   The roommate type
-  */
-case class OrderedStableMatching[A: Eq]()
-
 object OrderedStableMatching:
+  /**
+    * Used when the population matches to itself, as in the stable roommates problem
+    *
+    * Every member in the population is paired with someone else.
+    *
+    * @tparam A
+    *   The roommate type
+    */
   case class Total[A: Eq](population: List[A], mapping: Map[A, A]):
     assert(population.size == mapping.size)
 
+  /**
+    * Used when the population matches to itself, as in the stable roommates problem
+    *
+    * Unlike its total counterpart, not every member in the population may be linked to a another.
+    *
+    * @tparam A
+    *   The roommate type
+    */
   case class Partial[A: Eq](population: List[A], mapping: Map[A, A]):
     def withMatching(x: A, y: A): Either[String, Partial[A]] =
       Either
